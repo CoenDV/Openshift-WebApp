b12c6bf638bb5344e1ed7aae9f5cdc96732c043e
CoenDV <c.oen@hotmail.nl>
2024-09-13 16:13:50 +0200

    AI

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <p>{{ msg }}</p>
+  <p>{{ aiMsg }}</p>
 </template>
 
 <script>
@@ -16,14 +17,39 @@ export default {
   mounted() {
     axios.get("https://openshift-test-git-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/api")
       .then(response => {
-        this.msg = response.data;})
+        this.msg = response.data;
+      })
       .catch(error => {
         console.log(error);
       });
+
+    axios.post("https://accb-2001-1c04-4200-9500-e8a5-8873-9c6f-9ac.ngrok-free.app/v1/chat/completions",
+      {
+        "messages": [
+          {
+            "content": "You are a helpful assistant.",
+            "role": "system"
+          },
+          {
+            "content": "What is the capital of France?",
+            "role": "user"
+          }
+        ]
+      }
+    )
+      .then(response => {
+        console.log(response.data);
+        this.aiMsg = response.data.messages[0].content;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
   },
   data() {
     return {
-      msg: ''
+      msg: '',
+      aiMsg: ''
     }
   }
 }
