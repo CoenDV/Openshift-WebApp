63fd92edf3e800ccbe5b8012f1a968dc2b1d5138
CoenDV <c.oen@hotmail.nl>
2024-09-17 16:32:25 +0200

    Refactor API endpoint in App.vue and update response data assignment

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
   <p>{{ msg }}</p>
+
+  <h1>Ask a question: </h1>
+  <input type="text" v-model="question" />
+  <button @click="askQuestion">Ask</button>
+  <h1>AI response:</h1>
   <p>{{ aiMsg }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    
   },
   mounted() {
     axios.get("https://openshift-test-git-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/api")
@@ -22,25 +25,28 @@ export default {
       .catch(error => {
         console.log(error);
       });
-
-    axios.post("https://saved-ferret-rapid.ngrok-free.app/generate/",
-      {
-        "prompt": "what is the capital of france?"
-      }
-    )
-      .then(response => {
-        console.log(response.data);
-        this.aiMsg = response.data.response[0].message.content;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
   },
   data() {
     return {
       msg: '',
-      aiMsg: ''
+      aiMsg: '',
+      question: ''
+    }
+  },
+  methods: {
+    askQuestion() {
+      axios.post("https://saved-ferret-rapid.ngrok-free.app/generate/",
+        {
+          "prompt": this.question
+        }
+      )
+        .then(response => {
+          console.log(response.data);
+          this.aiMsg = response.data.response[0].message.content;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 }
